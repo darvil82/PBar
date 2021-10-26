64240a80ae128e6ac4db59cff341b61af3bd2cc0
DarviL82 <davidlosantos89@gmail.com>
2021-10-26 02:15:07 +0200

    Revert ""
    This reverts commit deefb5089aa9c2b3c4be2d27b17db482fb986e03.

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 import setuptools
-
-VERSION = "1.11.5"
-
-
-
+from src.pbar import __version__ as prjVersion
 
 with open("README.md", "r", encoding="utf-8") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
 	name="PBar2",
-	version=VERSION,
+	version=prjVersion,
 	author="David Losantos (DarviL82)",
 	author_email="davidlosantos89@gmail.com",
 	description="Display customizable progress bars on the terminal easily.",
