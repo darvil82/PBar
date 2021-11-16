4aaf6a8fed672c8f63ec48d97d9b45ebf7d19fef
DarviL82 <davidlosantos89@gmail.com>
2021-11-16 01:33:00 +0100

    added favicon

--- docs/genHtml.py
+++ docs/genHtml.py
@@ -23,6 +23,7 @@ HTML_CONTENT = """\
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title> PBar - {bannerSubtitle} </title>
+	<link rel="icon" href="images/favicon.png">
 	<link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
