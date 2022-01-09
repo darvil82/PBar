f8fc9b0377a161ebda0fe2cb05c4185dc1ccef8d
DarviL82 <davidlosantos89@gmail.com>
2022-01-09 04:27:20 +0100

    update version

--- src/pbar/__init__.py
+++ src/pbar/__init__.py
@@ -9,7 +9,7 @@
 - [Website](https://darvil82.github.io/PBar)
 """
 
-__version__ = "1.17.0"
+__version__ = "2.0.0"
 
 from . bar import PBar, iter, animate, bar_helper
 from . task_wrapper import task_wrapper
